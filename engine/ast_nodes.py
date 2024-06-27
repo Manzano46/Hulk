e763c38fef16a46372d5a9abb2ed3648bcc0e17c
Roger Fuentes <fuentesroger65@gmail.com>
2024-06-27 19:00:50 -0400

    feat: :sparkles: Fixing grammar and adding AST nodes

---
+++ engine/ast_nodes.py
@@ -0,0 +1,338 @@
+
+class Node:
+    pass
+
+# A program node has declaration and expression nodes
+class ProgramNode(Node):
+    def __init__(self, statements):
+        self.statement = statements
+
+class StatementNode(Node):
+    pass
+        
+class DeclarationNode(StatementNode):
+    pass
+        
+class ExpressionNode(StatementNode):
+    pass
+
+# A declaration node can be a function declaration, a type declaration or a protocol declaration
+class FunctionDeclarationNode(DeclarationNode):
+    def __init__(self, idx, params, expr, return_type=None):
+        super().__init__()
+        if len(params) > 0:
+            self.params = params
+        else:
+            self.params = []
+        self.id = idx
+        self.expr = expr
+        self.return_type = return_type
+
+class TypeDeclarationNode(DeclarationNode):
+    def __init__(self, idx, params, body, parent=None, parent_args=None):
+        super().__init__()
+        if len(params) > 0:
+            self.params = params
+        else :
+            self.params = []
+            
+        self.idx = idx
+        self.methods = [method for method in body if isinstance(method, MethodDeclarationNode)]
+        self.attributes = [attribute for attribute in body if isinstance(attribute, AttributeDeclarationNode)]
+        self.parent = parent
+        self.parent_args = parent_args
+
+class ProtocolDeclarationNode(DeclarationNode):
+    def __init__(self, idx, methods_signature, parent):
+        super().__init__()
+        self.idx = idx
+        self.methods_signature = methods_signature
+        self.parent = parent
+
+# Other declarations
+class VarDeclarationNode(DeclarationNode):
+    def __init__(self, idx, expr, var_type=None):
+        super().__init__()
+        self.id = idx
+        self.expr = expr
+        self.var_type = var_type
+
+
+class MethodDeclarationNode(DeclarationNode):
+    def __init__(self, idx, params, expr, return_type=None):
+        super().__init__()
+        if len(params) > 0:
+            self.params = params
+        else:
+            self.params = []
+        self.id = idx
+        self.expr = expr
+        self.return_type = return_type
+
+
+class MethodSignatureDeclarationNode(DeclarationNode):
+    def __init__(self, idx, params, return_type):
+        super().__init__()
+        if len(params) > 0:
+            self.params = params
+        else:
+            self.params = []
+        self.id = idx
+        self.return_type = return_type
+
+class AttributeDeclarationNode(DeclarationNode):
+    def __init__(self, idx, expr, attribute_type=None):
+        super().__init__()
+        self.id = idx
+        self.expr = expr
+        self.attribute_type = attribute_type
+
+# An expression can be a special expression (let,if,while,for), a block expression or a simple expression
+# Special expressions
+
+class LetInNode(ExpressionNode):
+    def __init__(self, var_declarations, expr):
+        super().__init__()
+        self.var_declarations = var_declarations
+        self.expr = expr
+
+class ConditionalNode(ExpressionNode):
+    def __init__(self, condition_expression_list, else_expr):
+        super().__init__()
+        self.condition_expression_list = condition_expression_list
+        self.else_expr = else_expr
+
+class WhileNode(ExpressionNode):
+    def __init__(self, condition, expression):
+        super().__init__()
+        self.condition = condition
+        self.expression = expression
+
+class ForNode(ExpressionNode):
+    def __init__(self, var, iterable, expression):
+        super().__init__()
+        self.var = var
+        self.iterable = iterable
+        self.expression = expression
+
+class ExpressionBlockNode(ExpressionNode):
+    def __init__(self, expressions):
+        super().__init__()
+        self.expressions = expressions
+
+# Simple expressions
+class DestructiveAssignmentNode(ExpressionNode):
+    def __init__(self, var, expr):
+        super().__init__()
+        self.var = var
+        self.expr = expr
+
+class IsNode(ExpressionNode):
+    def __init__(self, expression, ttype):
+        super().__init__()
+        self.expression = expression
+        self.ttype = ttype
+
+
+class AsNode(ExpressionNode):
+    def __init__(self, expression, ttype):
+        super().__init__()
+        self.expression = expression
+        self.ttype = ttype
+
+
+class FunctionCallNode(ExpressionNode):
+    def __init__(self, idx, args):
+        super().__init__()
+        self.idx = idx
+        self.args = args
+
+class IndexingNode(ExpressionNode):
+    def __init__(self, obj, index):
+        super().__init__()
+        self.obj = obj
+        self.index = index
+
+
+class TypeInstantiationNode(ExpressionNode):
+    def __init__(self, idx, args):
+        super().__init__()
+        self.idx = idx
+        self.args = args
+
+class AttributeCallNode(ExpressionNode):
+    def __init__(self, obj, attribute):
+        super().__init__()
+        self.obj = obj
+        self.attribute = attribute
+
+class VectorInitializationNode(ExpressionNode):
+    def __init__(self, elements):
+        super().__init__()
+        self.elements = elements
+
+class VectorComprehensionNode(ExpressionNode):
+    def __init__(self, selector, var, iterable):
+        super().__init__()
+        self.selector = selector
+        self.var = var
+        self.iterable = iterable
+
+class MethodCallNode(ExpressionNode):
+    def __init__(self, obj, method, args):
+        super().__init__()
+        self.obj = obj
+        self.method = method
+        self.args = args
+
+class BaseCallNode(ExpressionNode):
+    def __init__(self, args):
+        super().__init__()
+        self.args = args
+        self.method_name = None
+        self.parent_type = None
+
+
+
+class BinaryExpressionNode(ExpressionNode):
+    def __init__(self, left, right):
+        super().__init__()
+        self.left = left
+        self.right = right
+        self.operator = None
+
+
+class UnaryExpressionNode(ExpressionNode):
+    def __init__(self, operand):
+        super().__init__()
+        self.operand = operand
+        self.operator = None
+
+
+
+class PrintNode(ExpressionNode):
+    def __init__(self, expr):
+        self.expr = expr
+
+class AtomicNode(ExpressionNode):
+    def __init__(self, lex):
+        self.lex = lex
+
+
+class StrBinaryExpressionNode(BinaryExpressionNode):
+    pass
+
+
+class BoolBinaryExpressionNode(BinaryExpressionNode):
+    pass
+
+
+class InequalityExpressionNode(BinaryExpressionNode):
+    pass
+
+
+class ArithmeticExpressionNode(BinaryExpressionNode):
+    pass
+
+
+class EqualityExpressionNode(BinaryExpressionNode):
+    pass
+
+
+class NotNode(UnaryExpressionNode):
+    pass
+
+
+class NegNode(UnaryExpressionNode):
+    pass
+
+
+
+class ConcatNode(StrBinaryExpressionNode):
+    def __init__(self, left, right):
+        super().__init__(left, right)
+        self.operator = 'concat (@, @@)'
+
+
+class OrNode(BoolBinaryExpressionNode):
+    def __init__(self, left, right):
+        super().__init__(left, right)
+        self.operator = '|'
+
+
+class AndNode(BoolBinaryExpressionNode):
+    def __init__(self, left, right):
+        super().__init__(left, right)
+        self.operator = '&'
+
+
+class LessThanNode(InequalityExpressionNode):
+    def __init__(self, left, right):
+        super().__init__(left, right)
+        self.operator = '<'
+
+
+class GreaterThanNode(InequalityExpressionNode):
+    def __init__(self, left, right):
+        super().__init__(left, right)
+        self.operator = '>'
+
+
+class LessOrEqualNode(InequalityExpressionNode):
+    def __init__(self, left, right):
+        super().__init__(left, right)
+        self.operator = '<='
+
+
+class GreaterOrEqualNode(InequalityExpressionNode):
+    def __init__(self, left, right):
+        super().__init__(left, right)
+        self.operator = '>='
+
+
+class PlusNode(ArithmeticExpressionNode):
+    def __init__(self, left, right):
+        super().__init__(left, right)
+        self.operator = '+'
+
+
+class MinusNode(ArithmeticExpressionNode):
+    def __init__(self, left, right):
+        super().__init__(left, right)
+        self.operator = '-'
+
+
+class StarNode(ArithmeticExpressionNode):
+    def __init__(self, left, right):
+        super().__init__(left, right)
+        self.operator = '*'
+
+
+class DivNode(ArithmeticExpressionNode):
+    def __init__(self, left, right):
+        super().__init__(left, right)
+        self.operator = '/'
+
+
+class ModNode(ArithmeticExpressionNode):
+    def __init__(self, left, right):
+        super().__init__(left, right)
+        self.operator = '%'
+
+
+class PowNode(ArithmeticExpressionNode):
+    def __init__(self, left, right, operator):
+        super().__init__(left, right)
+        self.operator = operator
+
+
+class EqualNode(EqualityExpressionNode):
+    def __init__(self, left, right):
+        super().__init__(left, right)
+        self.operator = '=='
+
+
+class NotEqualNode(EqualityExpressionNode):
+    def __init__(self, left, right):
+        super().__init__(left, right)
+        self.operator = '!='
--- engine/grammar.py
+++ engine/grammar.py
@@ -1,58 +1,130 @@
 from cmp.pycompiler import Grammar
+from ast_nodes import *
 G = Grammar()
 
-program = G.NonTerminal('<program>', startSymbol=True)
-stat_list, stat, decl = G.NonTerminals('<stat_list> <stat> <decl>')
-def_func, print_stat = G.NonTerminals('<def-func> <print-stat>')
-expr, term, factor = G.NonTerminals('<expr> <term> <factor>')
-func_decl, meth, params, param, body, block, type_decl, protocol_decl, expr_list, comp_exp = G.NonTerminals('<func-decl> <meth> <params> <param> <body> <block> <type_decl> <protocol_decl> <expr-list> <comp_exp>')
-feature_list, feature, assignment, assignment_list, var, inherits, args, pro_meths, pro_meth, id_id_list = G.NonTerminals('<feature_list> <feature> <assignment> <assignment_list> <var> <inherits> <args> <pro_meths> <pro_meth> <id_id_list>')
-let_exp, if_exp, loop_exp, boolean_exp, arithmetic_exp, assignment_exp, vector_exp, atomic_exp, rest_if = G.NonTerminals('<let_exp> <if_exp> <loop_exp> <boolean_exp> <arithmetic_exp> <assignment_exp> <vector_exp> <atomic_exp> <rest_if>')
-
-let, printx, func, type_, protocol, extends, is_, as_, concat, in_, if_, elif_, else_, while_, for_, range, new = G.Terminals('let print function type protocol extends is as @@ in if elif else while for range new')
+# Terminals :
+    # reserved words
+let, printx, func, type_, protocol, extends, is_, as_, concat, concat_sp, in_, if_, elif_, else_, while_, for_, range, new, base = G.Terminals('let print function type protocol extends is as @ @@ in if elif else while for range new base')
+    # symbols
 semi, comma, opar, cpar, arrow, colon, semi, opcur, clcur, opcor, clcor, dot, com = G.Terminals('; , ( ) -> : ; { } [ ] . "')
-or_, andp, not_ = G.Terminals('| & !')
-equal, plus, minus, star, div, greater, less, mod, pot = G.Terminals('= + - * / > < % ^')
+
+    # operators
+equal, plus, minus, star, div, greater, less, mod, pot, or_, andp, not_ = G.Terminals('= + - * / > < % ^ | & !')
+
+    # 
 idx, num, string, boolean = G.Terminals('id int string boolean')
 
-program %= stat_list
 
-stat_list %= stat + semi # statement_list -> statement ,
-stat_list %= stat + semi + stat_list # statment_list -> statement , statement ...
+# NonTerminals :
+    #start symbol
+program = G.NonTerminal('<program>', startSymbol=True)
 
-stat %= decl # statement -> declaration
-stat %= expr #            | expression
+stat_list, stat, decl, special_exp = G.NonTerminals('<stat_list> <stat> <decl> <special_exp>')
 
-decl %= func_decl # declaration -> function_declaration
-decl %= type_decl #               |type_declaration
-decl %= protocol_decl #           |protocol_declaration
+def_func, print_stat = G.NonTerminals('<def-func> <print-stat>')
 
-func_decl %= func + meth # function_declaration -> function method
+expr, term, factor = G.NonTerminals('<expr> <term> <factor>')
 
-meth %= idx + opar + params + cpar + body # method_declaration -> name (params) body
-meth %= idx + opar + params + cpar + idx + body # method_declaration -> name (params) return_type body
+func_decl, meth, params, param, body, block, type_decl, protocol_decl, expr_list, comp_exp,var_exp = G.NonTerminals('<func-decl> <meth> <params> <param> <body> <block> <type_decl> <protocol_decl> <expr-list> <comp_exp> <var_exp>')
 
-params %= param # params -> param
-params %= param + comma + params # params -> param , param ....
+feature_list, feature, assignment, assignment_list, var, inherits, args, pro_meths, pro_meth, id_id_list, is_as_exp, concat_exp, pow_exp, type_exp, not_exp = G.NonTerminals('<feature_list> <feature> <assignment> <assignment_list> <var> <inherits> <args> <pro_meths> <pro_meth> <id_id_list> <is_as_exp> <concat_exp> <pow_exp> <type_exp> <not_exp>')
 
-param %= idx # param -> name
-param %= idx + colon + idx # param -> name : type
-param %= G.Epsilon 
+let_exp, if_exp, loop_exp, boolean_exp, arithmetic_exp, assignment_exp, vector_exp, atomic_exp, rest_if = G.NonTerminals('<let_exp> <if_exp> <loop_exp> <boolean_exp> <arithmetic_exp> <assignment_exp> <vector_exp> <atomic_exp> <rest_if>')
+
+# Productions
+
+program %= stat_list, lambda h,s : ProgramNode(s[1])  # program -> statement_list
+
+stat_list %= stat, lambda h,s : StatementNode(s[1])  # statement_list -> statement ,
+stat_list %= stat + stat_list  # statment_list -> statement , statement ...
+
+stat %= special_exp # statement -> special expression
+stat %= expr + semi #            | expression ;
+stat %= block #                  | {expression; expression...} 
+stat %= decl  #                  | declaration
+
+expr %= block # expression -> { expression; expression; ...}
+expr %= special_exp #       | special_expression
+
+special_exp %= let_exp #             | let_expression
+special_exp %= if_exp #              | if_expression
+special_exp %= loop_exp #            | loop_expresion (while or for)
+special_exp %= assignment_exp  #     | assignment_expression 
+
+block %= opcur + expr_list + clcur # block -> { expression; expression ...}
 
 body %= equal + greater + expr + semi # body -> => expression ;
 body %= block # body -> { block }
 
-block %= opcur + expr_list + clcur # block +> { expression; expression ...}
-
-type_decl %= type_ + idx + opcur + feature_list + clcur # type_declaration -> type name { assignments and methods}
-type_decl %= type_ + idx + opar + params + opcur + feature_list + clcur # type_declaration -> type name (params) { assignments and methods}
-type_decl %= type_ + idx + inherits + idx + opcur + feature_list + clcur # type_declaration -> type name inherits name { assignments and methods}
-type_decl %= type_ + idx + inherits + opar + args + cpar + opcur + feature_list + clcur # type_declaration -> type name inherits name (args) { assignments and methods}
-type_decl %= type_ + idx + opar + params + cpar + inherits + idx + opcur + feature_list + clcur # type_declaration -> type name (params) inherits name { assignments and methods}
-type_decl %= type_ + idx + opar + params + cpar + inherits + idx + opar + args + cpar + opcur + feature_list + clcur # type_declaration -> type name (params) inherits name (args) { assignments and methods}
-
 args %= expr # args -> expression
 args %= expr + comma + args # args -> expression , expression ...
+
+assignment_exp %= var + colon + equal + boolean_exp # assignment_expression -> variable := expression
+assignment_exp %= boolean_exp #                                              |boolean_exp
+
+boolean_exp %= boolean_exp + or_ + or_ + comp_exp # boolean_expression -> boolean_expression || comparation_expression
+boolean_exp %= boolean_exp + andp + andp + comp_exp #                    |boolean_expression && comparation_expression
+boolean_exp %= comp_exp #                                                |comparation_expression
+
+comp_exp %= comp_exp + equal + equal + is_as_exp # comparation_expression -> comparation_expresion == comparation_expression
+comp_exp %= comp_exp + not_ + equal + is_as_exp #                          |comparation_expression != comparation_expression
+comp_exp %= comp_exp + greater + is_as_exp #                               |comparation_expression > comparation_expression
+comp_exp %= comp_exp + less + is_as_exp #                                  |comparation_expression < comparation_expression
+comp_exp %= comp_exp + greater + equal + is_as_exp #                       |comparation_expression >= comparation_expression
+comp_exp %= comp_exp + less + equal + is_as_exp #                          |comparation_expression <= comparation_expression
+comp_exp %= is_as_exp #                                                    |is_as_expression
+
+is_as_exp %= is_as_exp + is_ + idx # is/as_expression -> is/as_expression is type
+is_as_exp %= is_as_exp + as_ + idx #                   | is/as_expression as type
+is_as_exp %= concat_exp #                              |concatenation_expression
+
+concat_exp %= concat_exp + concat + arithmetic_exp # concatenation_expression -> concatenation_expression @ arithmetic_expression
+concat_exp %= concat_exp + concat_sp + arithmetic_exp #                        | concatenation_expression @@ arithmetic_expression
+concat_exp %= arithmetic_exp #                                                 | arithmetic_expression
+
+arithmetic_exp %= arithmetic_exp + plus + term # arithmetic_expression -> arithmetic_expression + term
+arithmetic_exp %= arithmetic_exp + minus + term #                       | arithmetic_expression - term
+arithmetic_exp %= term #                                                | term
+
+term %= term + star + factor # term -> term * factor
+term %= term + div + factor #        | term / factor
+term %= term + mod + factor #        | term % factor
+term %= factor #                     | factor
+
+factor %= minus + pow_exp # factor -> - pow_operation
+factor %= pow_exp #                 | pow_operation
+
+pow_exp %= pow_exp + star + star + type_exp # pow_expression -> pow_expression ** type_expression
+pow_exp %= pow_exp + pot + type_exp #                         | pow_expression ^ type_expression
+pow_exp %= type_exp #                                         | type_expression
+
+type_exp %= new + idx + opar + cpar # type_expression -> new type ( )
+type_exp %= new + idx + opar + args + cpar #           | new type ( arg , arg ...)
+type_exp %= not_exp #                                  | not_expression
+
+not_exp %= not_ + var_exp # not_expression -> ! var_expression
+not_exp %= var_exp #                        | var_expression
+
+var_exp %= var + opar + cpar # var_expression -> name ()
+var_exp %= var + opar + args + cpar #          | name (arg, arg ...)
+var_exp %= atomic_exp #                        | atomic_expression
+
+var %= var + opcor + expr + clcor # variable -> name[expression]
+var %= var + dot + var # variable -> name.attribute
+var %= idx # variable -> name
+
+atomic_exp %= opar + expr + cpar # atomic_expression -> (expression)
+atomic_exp %= num #                                   | number
+atomic_exp %= boolean #                               | boolean
+atomic_exp %= string #                                | string
+atomic_exp %= var #                                   | name.attribute
+atomic_exp %= vector_exp #                            | vector_expression
+atomic_exp %= base + opar + cpar #                    | base  ()
+atomic_exp %= base + opar + args + cpar #             | base (expression,expression...)
+
+vector_exp %= idx + opcor + clcor # vector -> type [ ]
+vector_exp %= opcor + args + clcor #        | [expression, expression ...]
+vector_exp %= opcor + expr + or_ + or_ + idx + in_ + expr + clcor # vector -> [expression || id in expression]
 
 assignment %= idx + equal + expr # assignment -> name = expression
 assignment %= idx + colon + idx + equal + expr # assignment -> name : type = expression
@@ -63,6 +135,34 @@ feature_list %= feature + feature_list # feature_list -> feature feature ...
 feature %= assignment + semi # feature -> assignment ;
 feature %= meth # feature -> method
 
+
+# Declarations of functions, types and methods-----------------------------------------------------------------------------
+decl %= func_decl # declaration -> function_declaration
+decl %= type_decl #               |type_declaration
+decl %= protocol_decl #           |protocol_declaration
+
+# Functions and methods ----------------------------------------------------------------
+func_decl %= func + meth # function_declaration -> function method
+
+meth %= idx + opar + params + cpar + body # method_declaration -> name (params) body
+meth %= idx + opar + params + cpar + idx + body #               | name (params) return_type body
+
+params %= param # params -> param
+params %= param + comma + params # params -> param , param ....
+
+param %= idx # param -> name
+param %= idx + colon + idx # param -> name : type
+param %= G.Epsilon 
+
+# Types ----------------------------------------------------------------
+type_decl %= type_ + idx + opcur + feature_list + clcur # type_declaration -> type name { assignments and methods}
+type_decl %= type_ + idx + opar + params + opcur + feature_list + clcur # type_declaration -> type name (params) { assignments and methods}
+type_decl %= type_ + idx + inherits + idx + opcur + feature_list + clcur # type_declaration -> type name inherits name { assignments and methods}
+type_decl %= type_ + idx + inherits + opar + args + cpar + opcur + feature_list + clcur # type_declaration -> type name inherits name (args) { assignments and methods}
+type_decl %= type_ + idx + opar + params + cpar + inherits + idx + opcur + feature_list + clcur # type_declaration -> type name (params) inherits name { assignments and methods}
+type_decl %= type_ + idx + opar + params + cpar + inherits + idx + opar + args + cpar + opcur + feature_list + clcur # type_declaration -> type name (params) inherits name (args) { assignments and methods}
+
+# Protocoles
 protocol_decl %= protocol + idx + opcur + clcur # protocol -> protocol name {}
 protocol_decl %= protocol + idx + opcur + pro_meths + clcur # protocol -> protocol name {meth meth ...}
 protocol_decl %= protocol + idx + extends + idx + opcur + pro_meths + clcur # protocol -> protocol name {meth meth ...}
@@ -76,76 +176,20 @@ pro_meth %= idx + opar + id_id_list + cpar + colon + idx # pro_meth -> name ( na
 id_id_list %= idx + colon + idx # id_id_list -> name : type
 id_id_list %= idx + colon + idx + comma + id_id_list # id_id_list -> name : type, name : type , ...
 
-expr %= let_exp
-expr %= if_exp
-expr %= loop_exp
-expr %= block
-expr %= boolean_exp
-expr %= expr + is_ + idx
-expr %= expr + concat + expr
-expr %= arithmetic_exp
-expr %= expr + as_ + idx
-expr %= assignment_exp
-expr %= vector_exp
-expr %= atomic_exp
-
+# Let in ------------------------------------------------------------------------------------
 let_exp %= let + assignment_list + in_ + expr
 
 assignment_list %= assignment
 assignment_list %= assignment + assignment_list
 
+# Conditional --------------------------------------------------------------------------------
 if_exp %= if_ + opar + boolean_exp + cpar + expr + rest_if # if_exp -> if (boolean_exp) expression elif ... elif else expression
 rest_if %= elif_ + opar + expr + cpar + expr + rest_if # rest_if -> elif (expression) expression elif ...
 rest_if %= else_ + expr # rest_if -> else expression
 
+# Loop ---------------------------------------------------------------------------------------
 loop_exp %= while_ + opar + boolean_exp + cpar + expr # loop expression -> while (boolean_exp) expression
 loop_exp %= for_ + idx + in_ + range + opar + expr + comma +  expr + cpar + expr # for id in range(exp,exp) expression
-
-boolean_exp %= boolean_exp + or_ + or_ + comp_exp
-boolean_exp %= boolean_exp + andp + andp + comp_exp
-boolean_exp %= boolean_exp + equal + equal + comp_exp
-boolean_exp %= not_ + comp_exp
-boolean_exp %= comp_exp
-boolean_exp %= boolean
-
-comp_exp %= arithmetic_exp + greater + arithmetic_exp
-comp_exp %= arithmetic_exp + less + arithmetic_exp
-comp_exp %= arithmetic_exp + greater + equal + arithmetic_exp
-comp_exp %= arithmetic_exp + less + equal + arithmetic_exp
-comp_exp %= arithmetic_exp
-
-arithmetic_exp %= arithmetic_exp + plus + term
-arithmetic_exp %= arithmetic_exp + minus + term
-arithmetic_exp %= minus + term
-arithmetic_exp %= term
-
-term %= term + star + factor
-term %= term + div + factor
-term %= term + mod + factor
-term %= factor
-
-factor %= factor + star + star + atomic_exp
-factor %= factor + pot + atomic_exp
-factor %= atomic_exp
-
-assignment_exp %= var + colon + equal + expr # assignment_expression -> variable := expression
-
-var %= var + opcor + expr + clcor # variable -> name[expression]
-var %= var + dot + var # variable -> name.attribute
-var %= idx # variable -> name
-
-vector_exp %= opcor + clcor # vector -> [ ]
-vector_exp %= opcor + args + clcor # vector -> [expression, expression ...]
-vector_exp %= opcor + expr + or_ + or_ + expr + in_ + expr + clcor # vector -> [expression || expression in expression]
-
-atomic_exp %= new + idx + opar + cpar # atomic_expression -> new type ( )
-atomic_exp %= new + idx + opar + args + cpar # atomic_expression -> new type ( arg , arg ...)
-atomic_exp %= num
-atomic_exp %= com + string + com
-atomic_exp %= boolean
-atomic_exp %= var + opar + cpar # atomic_expression -> name ()
-atomic_exp %= var + opar + args + cpar # atomic_expression -> name (arg, arg ...)
-atomic_exp %= opar + expr + cpar # atomic_expression -> ( expression )
 
 
 
